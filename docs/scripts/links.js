6bd547709780d6a8d50e24b53d3cf7ad649f0ebc
Phil Kemp <boondock.dragon@gmail.com>
2026-08-04 09:55:00 +0200

    04 August 2026

--- docs/scripts/links.js
+++ docs/scripts/links.js
@@ -3,31 +3,37 @@ console.clear();
 if ( ! knownLinks ) {
     console.error("Did not load known links");
 } else {
-    links = document.querySelectorAll("#bodyContent a");
+    links = document.querySelectorAll("#bodyContent a:not(.external)");
 
     found = 0;
 
     links.forEach(l=>{
         if ( found !== 20 ) {
             href = l.getAttribute("href");
-            if ( href.startsWith("//en.wikipedia.org/wiki/") ) {
-                href = href.replace("//en.wikipedia.org/wiki/","");
+            if ( href.startsWith("https://en.wikipedia.org/wiki/") ) {
+                href = href.replace("https://en.wikipedia.org/wiki/","");
 
-               if ( href.endsWith("?action=edit&redlink=1") ) {
+                if ( href.endsWith("?action=edit&redlink=1") ) {
                     href = href.replace("?action=edit&redlink=1","");
                     if ( ! redLinks.includes(href) ) {
                         href = "\x1B[31;1073;1mredlink: \x1B[0m" + href;
                         console.log(href);
                         found++;
                     }
+                } else if ( href.statsWith("Template_talk:") || href.startsWith("Special:EditPage/Template") ) {
+                    // template talk/edit page, ignore
                 } else {
                     if ( ! knownLinks.includes(href) ) {
                         console.log(href);
                         found++;
                     }
                 }
-            } else if ( href.startsWith("#cite_note-") ) {
+            } else if ( href.startsWith("#cite_note-") || href.startsWith("#cite_ref-") ) {
                 // citation link, ignore
+            } else if ( href.startsWith("#/map/") || href.endsWith("Maps_Terms_of_Use") || href.indexOf("www.openstreetmap.org") !== -1 || href.indexOf("geohack.toolforge.org") !== -1 ) {
+                // map interaction, ignore
+            } else if ( href.indexOf("commons.wikimedia.org") !== -1 ) {
+                // wiki commons, ignore
             } else {
                 if ( href.startsWith("/w/index.php?title=") && href.indexOf("&action=edit&section=") !== -1 ) {
                     // edit a section, ignore
